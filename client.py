ae46592f0f79a838e02e10e42445b74e5494fb6b
PrinceMargaret <princeagrahari2000@gmail.com>
2020-10-02 02:35:21 +0530

    serverclient

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+s=socket.socket()
+host=input("enter server host name or server ip address: ")
+
+port=1234
+s.connect((host,port))
+print('connected to server')
+while 1:
+    in_msg=s.recv(5000)
+    in_msg=in_msg.decode()
+    print("Server:>>",in_msg)
+    msg=input("You:>>")
+    msg=msg.encode()
+    s.send(msg)
+
+
+
---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+s=socket.socket()       #initialization of socket 
+
+host , port=socket.gethostname() , 1234   
+
+print("server will start on host : "+host)
+
+s.bind((host,port))   #bind the server with host and port
+print('Server is bound successfully')
+
+s.listen(1000)        #1000 define that 1000 device can be connect by this server
+conn,address=s.accept()   
+
+print(address," client has connected")
+while 1:
+    conn,address=s.accept()
+    msg=input("You:>>")
+    msg=msg.encode()
+    conn.send(msg) 
+    in_msg=s.recv(5000) 
+    in_msg=in_msg.decode()
+    print("Server:>>",in_msg)
+
